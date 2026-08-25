14450b84ba55ff98933f4a1acc6d6188908d1849
alvaro9rqc <alvaro9rqc@gmail.com>
2026-08-24 19:03:22 -0500

    feat: install portable theme reload hook

--- personal/caelestia/hypr-vars.lua
+++ personal/caelestia/hypr-vars.lua
@@ -1,6 +1,6 @@
 return {
     browser      = "brave",
-    editor       = "foot /home/alvaro9rqc/.local/share/bob/nvim-bin/nvim",
+    editor       = "foot $HOME/.local/share/bob/nvim-bin/nvim",
     kbShowPanels = "CTRL + SUPER + K",
     kbLock       = "CTRL + SUPER + L",
 }
